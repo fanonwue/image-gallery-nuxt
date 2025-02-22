3d2e60c8934274789fca44fb648d17685e40c870
Fabian Wünderich <fabian@wuenderich.de>
2025-02-22 23:41:26 +0100

    Cleanup migrations

--- prisma/migrations/20250222223337_initial/migration.sql
+++ prisma/migrations/20250222223337_initial/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "active" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -29,6 +31,26 @@ CREATE TABLE "ImageMeta" (
     CONSTRAINT "ImageMeta_imageId_fkey" FOREIGN KEY ("imageId") REFERENCES "Image" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "ownerId" TEXT NOT NULL,
+    CONSTRAINT "Folder_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ImagesInFolders" (
+    "imageId" INTEGER NOT NULL,
+    "folderId" INTEGER NOT NULL,
+    "assignedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("imageId", "folderId"),
+    CONSTRAINT "ImagesInFolders_imageId_fkey" FOREIGN KEY ("imageId") REFERENCES "Image" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "ImagesInFolders_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
@@ -36,7 +58,19 @@ CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
+CREATE INDEX "User_active_idx" ON "User"("active");
+
+-- CreateIndex
 CREATE INDEX "Image_ownerId_idx" ON "Image"("ownerId");
 
 -- CreateIndex
 CREATE INDEX "ImageMeta_imageId_idx" ON "ImageMeta"("imageId");
+
+-- CreateIndex
+CREATE INDEX "Folder_ownerId_idx" ON "Folder"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "ImagesInFolders_folderId_idx" ON "ImagesInFolders"("folderId");
+
+-- CreateIndex
+CREATE INDEX "ImagesInFolders_imageId_idx" ON "ImagesInFolders"("imageId");
