31f4c89ddde0ee75c0706f506bcfbee7869cf024
Fabian Wünderich <fabian@wuenderich.de>
2025-02-23 02:59:19 +0100

    Revert "Cache folders response for some time"
    This reverts commit e0129a6382ad285265857b7afc0a48af16f82da3.
    
    This implementation caused hydration mismatches, removing it for now

--- stores/folders.ts
+++ stores/folders.ts
@@ -1,32 +1,15 @@
-import {acceptHMRUpdate, defineStore, useFetch} from "#imports";
+import {acceptHMRUpdate, defineStore, useAsyncData, useRequestFetch} from "#imports";
 import type {FolderDto} from "#shared/dto";
-import type {NuxtApp} from "#app";
 
-// Set cache TTL to 60 seconds
-const foldersTtl = 60 * 1000
 
 export const useFoldersStore = defineStore('folders', () => {
-    const lastFetch = ref<Date|undefined>()
-    const foldersAsync = () => useFetch<FolderDto[]>("/api/folders", {
-        onResponse: () => {
-            lastFetch.value = new Date()
-        },
-        getCachedData: (key: string, nuxtApp: NuxtApp) => {
-            const data = nuxtApp.payload.data[key] || nuxtApp.static.data[key];
-            if (!data || !lastFetch.value) return;
 
-            const expirationDate = new Date(lastFetch.value)
-            expirationDate.setTime(expirationDate.getTime() + foldersTtl);
-
-            if (expirationDate.getTime() < Date.now()) {
-                return
-            }
-            return data
-        }
-    })
+    const requestFetch = useRequestFetch()
+    const fetchFolders = () => requestFetch<FolderDto[]>("/api/folders")
+    const foldersAsync = () => useAsyncData('folders', () => fetchFolders())
 
     return {
-        foldersAsync
+        fetchFolders, foldersAsync
     }
 })
 
